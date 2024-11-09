fa21b8af6fb8d882e49566466ec8931d62f2da42
Daviidls <david.limia0@gmail.com>
2024-11-09 18:29:22 +0100

    Actualización Tarea

--- Damas-main/Damas-main/src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ Damas-main/Damas-main/src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -3,5 +3,25 @@ package org.iesalandalus.programacion.damas.modelo;
 public class Posicion {
     private int fila;
     private char columna;
-    
+
+
+
+    private void setFila(int fila)
+    {
+        if (fila>8 || fila<1){
+            throw new IllegalArgumentException("El valor de la fila no puede ser menor de 1 y mayor de 8");
+        }
+            this.fila=fila;
+    }
+    public int getFila(){
+        return fila;
+    }
+    private void setColumna(char columna)
+    {
+        if (columna<'a' || columna> 'h'){
+            throw new IllegalArgumentException("El valor de la columna tiene que estar entre 'a' y 'h' ");
+        }
+        this.columna=columna;
+    }
+
 }
