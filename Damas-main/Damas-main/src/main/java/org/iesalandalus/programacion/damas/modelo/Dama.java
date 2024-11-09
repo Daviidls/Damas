3e006d1c2747761ecc74863104b5a4a60de23f77
Daviidls <david.limia0@gmail.com>
2024-11-09 20:39:14 +0100

    Actualización tarea

--- Damas-main/Damas-main/src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ Damas-main/Damas-main/src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -7,6 +7,16 @@ public class Dama {
     private Posicion posicion;
     private boolean esDamaEspecial;
 
+   public Dama()
+   {
+
+   }
+   public Dama(Color color)
+   {
+       this.color=Color.BLANCO; //La dama sera blanca.
+       this.posicion= crearPosicionInicial(); //Genera una posicion inicial en este caso siempre del color blanco.
+       this.esDamaEspecial = false;//Al principio no es dama especial.
+   }
     public Color getColor()
     {
         return color;
@@ -25,5 +35,22 @@ public class Dama {
     {
         this.posicion=Objects.requireNonNull(posicion,"La posición no puede ser nula");
     }
-    
+    private Posicion crearPosicionInicial()
+    {
+
+        int fila = (int) (Math.random() * 3) + 1;  // Aleatorio entre 1, 2, 3
+
+        char columna;
+        if (fila == 1 || fila == 3) {
+
+            columna = (char) ('A' + (Math.random() * 4) * 2);  // Aleatorio entre A, C, E, G
+        } else {
+
+            columna = (char) ('A' + (Math.random() * 4) * 2 + 1);  // Aleatorio entre B, D, F, H
+        }
+        return new Posicion(fila, columna);
+    }
+
+
+
 }
